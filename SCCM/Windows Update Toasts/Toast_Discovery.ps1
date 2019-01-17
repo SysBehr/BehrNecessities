725cca8693b05ba1ce32d1366131f70e1752eb02
SysBehr <wilkins.colin.c@gmail.com>
2019-01-17 14:24:22 -0500

    Update Toast_Discovery.ps1

--- SCCM/Windows Update Toasts/Toast_Discovery.ps1
+++ SCCM/Windows Update Toasts/Toast_Discovery.ps1
@@ -1,5 +1,6 @@
 #Discovery Script for SCCM Configuration Item
 #If updates are returned (count > 0), display the toast
+#Credit goes to @jgkps
 
 $VisibleUpdates = Get-CimInstance -Namespace Root\ccm\clientSDK -Class CCM_softwareupdate | Where-Object {$_.useruiexperience -eq $true}
 $VisibleUpdates.Name.Count
