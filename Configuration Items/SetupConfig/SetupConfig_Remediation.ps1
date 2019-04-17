c5c52f8548f8e5a99d2cda0342a733907be384e4
SysBehr <wilkins.colin.c@gmail.com>
2019-04-17 10:37:20 -0400

    Create SetupConfig_Remediation.ps1

---
+++ Configuration Items/SetupConfig/SetupConfig_Remediation.ps1
@@ -0,0 +1,25 @@
+#Variable for ini file path
+$iniFilePath = "$env:SystemDrive\Users\Default\AppData\Local\Microsoft\Windows\WSUS\SetupConfig.ini"
+
+#Variables for SetupConfig
+$iniSetupConfigSlogan = "[SetupConfig]"
+$iniSetupConfigKeyValuePair =@{"Priority"="Normal";"BitLocker"="AlwaysSuspend";"Compat"="IgnoreWarning"}
+
+#Init SetupConfig content
+$iniSetupConfigContent = @"
+$iniSetupConfigSlogan
+"@
+
+#Build SetupConfig content with settings
+foreach ($k in $iniSetupConfigKeyValuePair.Keys) 
+{
+    $val = $iniSetupConfigKeyValuePair[$k]
+
+    $iniSetupConfigContent = $iniSetupConfigContent.Insert($iniSetupConfigContent.Length, "`r`n$k=$val")
+}
+
+IF(Test-Path $iniFilePath){
+Remove-Item $iniFilePath -Force
+}
+#Write content to file 
+New-Item $iniFilePath -ItemType File -Value $iniSetupConfigContent -Force
