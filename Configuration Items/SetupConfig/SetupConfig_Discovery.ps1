4af3e6d3ca1449c5b9def9117b362cc68f66a876
SysBehr <wilkins.colin.c@gmail.com>
2019-04-17 11:04:15 -0400

    Create SetupConfig_Discovery.ps1

---
+++ Configuration Items/SetupConfig/SetupConfig_Discovery.ps1
@@ -0,0 +1,35 @@
+#Discovery SetupPriority
+
+IF(Get-Item "$env:SystemDrive\Users\Default\AppData\Local\Microsoft\Windows\WSUS\SetupConfig.ini" -ErrorAction SilentlyContinue)
+{
+$setupfile = (Get-Content C:\Users\Default\Appdata\Local\Microsoft\Windows\WSUS\SetupConfig.ini)
+
+Foreach($line in $setupfile){
+If($line -like 'Priority*'){return $line.replace("Priority=","")}
+}
+Return "NonCompliant"
+}
+
+#Discovery Compat
+
+IF(Get-Item "$env:SystemDrive\Users\Default\AppData\Local\Microsoft\Windows\WSUS\SetupConfig.ini" -ErrorAction SilentlyContinue)
+{
+$setupfile = (Get-Content C:\Users\Default\Appdata\Local\Microsoft\Windows\WSUS\SetupConfig.ini)
+
+Foreach($line in $setupfile){
+If($line -like 'Compat*'){return $line.replace("Compat=","")}
+}
+Return "NonCompliant"
+}
+
+#Discovery Bitlocker
+
+IF(Get-Item "$env:SystemDrive\Users\Default\AppData\Local\Microsoft\Windows\WSUS\SetupConfig.ini" -ErrorAction SilentlyContinue)
+{
+$setupfile = (Get-Content C:\Users\Default\Appdata\Local\Microsoft\Windows\WSUS\SetupConfig.ini)
+
+Foreach($line in $setupfile){
+If($line -like 'Bitlocker*'){return $line.replace("Bitlocker=","")}
+}
+Return "NonCompliant"
+}
