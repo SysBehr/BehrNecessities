ff9ef86e75cdb8ec7c35b676d56c8de9b53cb93e
SysBehr <wilkins.colin.c@gmail.com>
2019-08-06 13:46:12 -0400

    Task Sequence Toast Notifications
    For Task Sequence Deployment notifications

---
+++ ConfigItems/Task Sequence Toasts/README.md
@@ -0,0 +1,4 @@
+Instructions:
+- Test the scripts & modify with your branding.
+- Create a CI with the discovery and remediation scripts. Ensure it runs in the user context.
+- Compliance is an integer value and it should return 0 to be compliant since it's looking for deployed task sequences.
---
+++ ConfigItems/Task Sequence Toasts/TaskSequenceToast_Discovery.ps1
@@ -0,0 +1,13 @@
+﻿## Displays a Windows 10 Toast Notification for Task Sequences
+## SCCM Configuration Item Discovery Script
+## Set the Compliance setting as an Integer equaling Zero. There is handling for Required vs Available count in the Remediation script.
+# Author: Colin Wilkins @sysBehr
+# Modified: 08/06/2019
+# Version: 1.0
+
+$softwareCenter = New-Object -ComObject "UIResource.UIResourceMgr"
+
+$AvailableTSList = $Softwarecenter.GetAvailableApplications() | Where-Object {$_.IsAssigned -ne 1}
+$RequiredTSList = $Softwarecenter.GetAvailableApplications() | Where-Object {$_.IsAssigned -eq 1}
+
+Return ($AvailableTSList.Name.Count + $RequiredTSList.Name.Count)
