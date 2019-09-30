fbcc257a70e93f59e2bd91589095861625a219a1
SysBehr <wilkins.colin.c@gmail.com>
2019-09-30 11:44:23 -0400

    Update No_Interactive_User_Remediation.ps1

--- ConfigItems/No_Interactive_User/No_Interactive_User_Remediation.ps1
+++ ConfigItems/No_Interactive_User/No_Interactive_User_Remediation.ps1
@@ -1,3 +1,8 @@
-﻿Stop-Service wuauserv
-(Get-Item -Path C:\Windows\SoftwareDistribution\Download\*) | Remove-Item -Force -Recurse
+Stop-Service wuauserv -Force
+
+$badupdate = Get-ChildItem -Path C:\Windows\SoftwareDistribution\Download\ -Recurse -filter WindowsUpdateBox.exe
+If($badupdate){
+Remove-Item $badupdate.Directory -Recurse -Force
+}
+
 Start-Service wuauserv
